02cad7415f5d9caeb8dbc91e1f507b10ac835533
bryanleezh <leebryan307@gmail.com>
2023-12-20 23:53:16 +0800

    added navbar

---
+++ lib/data.ts
@@ -0,0 +1,21 @@
+import { Plane, CircleUserRound, GanttChartSquare } from "lucide-react";
+
+
+// data for navbar info
+export const navbarLinks = [
+    {
+        title: "Trips",
+        url: "/trips",
+        icon: Plane,
+    },
+    {
+        title: "Activity",
+        url: "/activity",
+        icon: GanttChartSquare,
+    },
+    {
+        title: "Account",
+        url: "/account",
+        icon: CircleUserRound,
+    },
+]
