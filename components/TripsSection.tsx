28b88cd01af11d2e844aae45783a7723b8df45b7
bryanleezh <leebryan307@gmail.com>
2023-12-25 00:54:09 +0800

    Feat: Add new component for Trips page

--- components/TripsSection.tsx
+++ components/TripsSection.tsx
@@ -5,6 +5,8 @@ import { AccountInfoProps } from '@/lib/types';
 
 export default function TripsSection ( { email } : AccountInfoProps ) {
   return (
-    <div>TripsSection</div>
+    <div className='mt-10 pt-10 flex justify-center'>
+        <p>TripsSection</p>
+    </div>
   )
 }
