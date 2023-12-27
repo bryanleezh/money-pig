b42d339b2cfd8b92805f634da4fa915b09a88aca
bryanleezh <leebryan307@gmail.com>
2023-12-28 00:40:10 +0100

    Chore: FIxed Navbar centering

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -18,7 +18,7 @@ export default function Nav() {
       
     return (
         <nav className='ml-0 fixed z-10'>
-            <ul className='flex flex-row gap-5 fixed left-8 right-8 justify-center *:sm:m-10 m-3 '>
+            <ul className='flex flex-row gap-5 fixed left-8 right-8 justify-around *:sm:m-10 m-3 '>
                 {navbarLinks.map((item, index) => (
                     <li key={index} className="mr-4">
                         <Link aria-label={item.title} href={item.url}>
