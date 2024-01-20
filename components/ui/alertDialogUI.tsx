900b2e4f5fb4019f111811b19d0cb672d5f85aea
bryanleezh <leebryan307@gmail.com>
2024-01-20 18:27:18 +0100

    Feat: Add AlertDialog UI

--- components/ui/alertDialogUI.tsx
+++ components/ui/alertDialogUI.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import * as AlertDialog from '@radix-ui/react-alert-dialog';
 
-const AlertDialogConfirmation = () => (
+const AlertDialogUI = () => (
   <AlertDialog.Root>
     <AlertDialog.Trigger asChild>
       <button className="text-violet11 hover:bg-mauve3 shadow-blackA4 inline-flex h-[35px] items-center justify-center rounded-[4px] bg-white px-[15px] font-medium leading-none shadow-[0_2px_10px] outline-none focus:shadow-[0_0_0_2px] focus:shadow-black">
@@ -35,4 +35,4 @@ const AlertDialogConfirmation = () => (
   </AlertDialog.Root>
 );
 
-export default AlertDialogConfirmation;
+export default AlertDialogUI;
