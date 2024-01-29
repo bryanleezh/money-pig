db61c0988d28bfbe2b9fcae716123b2ca4abeff7
bryanleezh <leebryan307@gmail.com>
2024-01-29 14:29:35 +0100

    Fix: Function declaration

--- lib/firebase/firestore/getData.ts
+++ lib/firebase/firestore/getData.ts
@@ -3,7 +3,7 @@ import firebase_app from "../config";
 import { getFirestore, doc, getDoc, DocumentData } from "firebase/firestore";
 
 const db = getFirestore(firebase_app)
-export default async function getDocument(collection: string, id: string) {
+export default async function getData(collection: string, id: string) {
     let docRef = doc(db, collection, id);
 
     let result: DocumentSnapshot | null = null;
