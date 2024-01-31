476ddb02be618db43ac975da6e3cc50e0d3b92bd
bryanleezh <leebryan307@gmail.com>
2024-01-31 11:08:26 +0100

    Chore: Remove comments

--- lib/algos/simplifyDebt.ts
+++ lib/algos/simplifyDebt.ts
@@ -1,8 +1,5 @@
 // TODO: Add Simplify Debt Algo here
 
-// * 3 ways to split: Exact, Equal, Percentage
-// * Can Create Expense class, extend the 3 ways out of Expense Class
-
 // * Function for simplifying all debts in a nested array of numbers 
 // @params transactions -> nested array of numbers [ ['from','to', 'amount'], ['from','to','amount'] ]
 
