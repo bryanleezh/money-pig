60bd638277604e762c78328e48bdc935aebc3697
bryanleezh <leebryan307@gmail.com>
2024-03-06 23:32:37 +0100

    Chore: Add comments

--- lib/addExpense.ts
+++ lib/addExpense.ts
@@ -1,6 +1,17 @@
 // * 3 ways to split: Exact, Equal, Percentage
 // * Can Create Expense class, extend the 3 ways out of Expense Class
 
-export default function addExpense() {
-    
+// * Params: 
+// * from, to -> user index in trip data
+// * totalAmt -> total amount of expense (used to compute how much to add based on expense type)
+// * type -> type of expense (for computing amt)
+export default function addExpense( from: number, to: number, totalAmt: number ,type: string ) {
+    // TODO: Add to firebase into a list of transactions??
+    if (type == "exact") {
+        // TODO
+    } else if (type == "equal") {
+        // TODO
+    } else {
+        // TODO
+    }
 }
