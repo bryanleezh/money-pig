6c07109543d70474df2158e8950a90490d8f35c0
bryanleezh <leebryan307@gmail.com>
2024-03-11 02:01:50 +0100

    Chore: Add todo

--- components/ExpensesView.tsx
+++ components/ExpensesView.tsx
@@ -5,7 +5,7 @@ import { TripUuid } from '@/lib/types';
 
 export default function ExpensesView( {tripUUID} : TripUuid ) {
     console.log(tripUUID);
-
+    // TODO: Populate with all expenses + total of each currency spent in each trip
     return (
         <div>ExpensesView</div>
     )
