40ca229eb7a235c21620c8bf82faeb21fc983c41
bryanleezh <leebryan307@gmail.com>
2024-03-12 17:00:53 +0100

    Chore: Add comments

--- lib/addExpense.ts
+++ lib/addExpense.ts
@@ -1,16 +1,17 @@
 // * 3 ways to split: Exact, Equal, Percentage
 // * Can Create Expense class, extend the 3 ways out of Expense Class
 
-import { FirestoreError, arrayUnion, doc, getFirestore, setDoc } from "firebase/firestore";
-import addData from "./firebase/firestore/addData";
+import { arrayUnion, doc, getFirestore, setDoc } from "firebase/firestore";
 import firebase_app from "./firebase/config";
 
 // * Params: 
 // * from, to -> user index in trip data
+// * from -> user that needs to pay
+// * to -> user that receives the money (the one that paid 1st)
 // * totalAmt -> total amount of expense (used to compute how much to add based on expense type)
 // ! type has been removed to be placed in a separate function
 export default async function addExpense( from: number, to: number, amount: number, uuid: string ) {
-    // TODO: Add to firebase under 'expenses' array
+    // TODO: Add to firebase under trips -> indiv user expense
     const db = getFirestore(firebase_app);
     const transaction = [from, to, amount];
     const tripDocRef = doc(db, 'trips', uuid);
@@ -22,7 +23,7 @@ export default async function addExpense( from: number, to: number, amount: numb
                 },
                 { merge: true }
             );
-            console.log(`Expense data added to trip: ${uuid}`);
+        console.log(`Expense data added to trip: ${uuid}`);
     } catch (err) {
         console.log("Error adding expense", err);
     }
