5961921f2a0e51ad96a14c85866a68eaea7ac62f
bryanleezh <leebryan307@gmail.com>
2024-03-21 23:05:04 +0100

    Feat: Add user expense to db

---
+++ lib/expenses/addUserExpense.ts
@@ -0,0 +1,20 @@
+// * Function that adds amount spent by person
+
+
+import { doc, getFirestore, updateDoc, increment } from "firebase/firestore";
+import firebase_app from "../firebase/config";
+
+export default async function addUserExpense(amount: number, personEmail: string, uuid: string, currency: string) {
+    const db = getFirestore(firebase_app);
+    const tripDocRef = doc(db, 'trips', uuid);
+
+    try {
+        await updateDoc(tripDocRef, {
+            [`usersExpense.${personEmail}.${currency}`]: increment(amount)
+        });
+        console.log(`Added ${amount} ${currency} spent by ${personEmail}`);
+    } catch (err) {
+        console.error("Error adding user expense: ", err);
+    }
+
+}
