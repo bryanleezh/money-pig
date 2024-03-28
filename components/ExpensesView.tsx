59c2c79413a4d0ddcd1fa8a48ebf26bff2d762d1
bryanleezh <leebryan307@gmail.com>
2024-03-28 22:31:10 +0100

    Feat: Add props to display trip data

--- components/ExpensesView.tsx
+++ components/ExpensesView.tsx
@@ -1,11 +1,14 @@
 'use client';
 
 import React from 'react';
-import { TripUuid } from '@/lib/types';
+import { TripInfo } from '@/lib/types';
 
-export default function ExpensesView( {tripUUID} : TripUuid ) {
+export default function ExpensesView( {tripUUID, tripData} : TripInfo ) {
     console.log(tripUUID);
+    console.log(tripData);
+
     // TODO: Populate with all expenses + total of each currency spent in each trip
+    // TODO: Add delete button for each expense (similar to deleteTrip component)
     return (
         <div>ExpensesView</div>
     )
