7456dfedcd1c5fdac9019344dabdbee1cf2ba544
bryanleezh <leebryan307@gmail.com>
2024-03-29 22:46:48 +0100

    Feat: Add total expense to account card

--- components/AccountCard.tsx
+++ components/AccountCard.tsx
@@ -9,6 +9,15 @@ export default function AccountCard ({
   tripCount,
   totalExpense
 } : AccountCardProps) {
+  
+  const totalExpenseElements = totalExpense ? Object.entries(totalExpense).map(([currency, expense]) => (
+    <div key={currency}>
+      <p className="text-base text-primary leading-relaxed text-body-color dark:text-cyan-200">
+        {currency}: {expense}
+      </p>
+    </div>
+  )) : null;
+
   return (
     <>
       <div className="mb-10 overflow-hidden rounded-lg bg-dark shadow-1 duration-300 hover:shadow-3 dark:bg-dark-2 dark:shadow-card dark:hover:shadow-3">
@@ -21,18 +30,19 @@ export default function AccountCard ({
         </div>
         <div className="p-8 text-center mt-2 sm:p-9 md:p-7 xl:p-9">
           <h3>
-            <a
-              className="mb-4 block text-xl font-semibold text-primary hover:text-primary dark:text-cyan-200 sm:text-[22px] md:text-xl lg:text-[22px] xl:text-xl 2xl:text-[22px]"
-            >
+            <a className="mb-4 block text-xl font-semibold text-primary hover:text-primary dark:text-cyan-200 sm:text-[22px] md:text-xl lg:text-[22px] xl:text-xl 2xl:text-[22px]">
               Hi {username}! This is what your expenses look like!
             </a>
           </h3>
           <p className="mb-7 text-base text-primary leading-relaxed text-body-color dark:text-cyan-200">
             Total Number of Trips: {tripCount}
           </p>
-          <p className="mb-7 text-base text-primary leading-relaxed text-body-color dark:text-cyan-200">
-            Total Amount Spent: ${totalExpense}
+          <p className="text-base text-primary leading-relaxed text-body-color dark:text-cyan-200">
+            Total Amount Spent:
           </p>
+          <div>
+            {totalExpenseElements}
+          </div>
         </div>
       </div>
     </>
