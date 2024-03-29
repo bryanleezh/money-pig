9960412df099966be005983b5e1782179461d6fd
bryanleezh <leebryan307@gmail.com>
2024-03-29 22:47:46 +0100

    Feat: Add trip data for display

--- components/ExpensesView.tsx
+++ components/ExpensesView.tsx
@@ -7,10 +7,22 @@ export default function ExpensesView( {tripUUID, tripData} : TripInfo ) {
     console.log(tripUUID);
     console.log(tripData);
 
+    const tripName = tripData?.name;
+    const expensesLog = tripData?.expensesLog;
+    const totalExpense = tripData?.totalExpense;
+    const usersExpense = tripData?.usersExpense;
+    const usersExpensePaid = tripData?.usersExpensePaid;
+
     // TODO: Populate with all expenses + total of each currency spent in each trip
     // TODO: Add delete button for each expense (similar to deleteTrip component)
     return (
-        <div>ExpensesView</div>
+        <div className='container mx-auto max-w-3xl p-8 grow'>
+            {/* Total Users Expense */}
+    
+            {/* Users Expense Paid */}
+            
+            {/* All Expenses */}
+        </div>
     )
 }
 
