7189096e4ce45a848768b9260e73380903c3927a
bryanleezh <leebryan307@gmail.com>
2024-03-30 22:50:48 +0100

    Feat: Add expense logging

--- lib/expenses/addExpenseLog.ts
+++ lib/expenses/addExpenseLog.ts
@@ -3,13 +3,44 @@
 import { arrayUnion, doc, getFirestore, updateDoc } from "firebase/firestore";
 import firebase_app from "../firebase/config";
 
-export default async function addExpenseLog(amount: number, description: string, uuid: string, currency: string, type: string) {
+export default async function addExpenseLog(amount: number, description: string, uuid: string, currency: string, type: string, userEmail: string, bestieEmail: string) {
     const db = getFirestore(firebase_app);
+    let paidFor: string[] = [];
+    let paidBy: string = "";
+
+    switch (type) {
+        case "equal":
+            paidFor = [userEmail, bestieEmail];
+            paidBy = userEmail;
+            break;
+        case "indiv":
+            paidFor = [userEmail];
+            paidBy = userEmail;
+            break;
+        case "bestie":
+            paidFor = [bestieEmail];
+            paidBy = userEmail;
+            break;
+        case "bestiepay":
+            paidFor = [bestieEmail];
+            paidBy = bestieEmail;
+            break;
+        case "bestiepayforyou":
+            paidFor = [userEmail];
+            paidBy = bestieEmail;
+            break;
+        default:
+            paidFor = [];
+            break;
+    }
+
     const log = {
         "desc": description,
         "log": {
             "amount": amount,
-            "currency": currency
+            "currency": currency,
+            "paidFor": paidFor,
+            "paidBy": paidBy,
         },
         "timeStamp": new Date(),
         "transactionType": type
