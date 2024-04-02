634498d8477d818d0ddfb92736047e4f2bda899f
bryanleezh <leebryan307@gmail.com>
2024-04-02 13:56:06 +0200

    Feat: Add total expense display

--- components/ExpensesView.tsx
+++ components/ExpensesView.tsx
@@ -13,21 +13,37 @@ export default function ExpensesView( {tripUUID, tripData} : TripInfo ) {
     const usersExpense = tripData?.usersExpense;
     const usersExpensePaid = tripData?.usersExpensePaid;
 
+    const totalExpenseElements = totalExpense ? Object.entries(totalExpense).map(([currency, expense]) => (
+        <div key={currency}>
+            <p className="text-base text-primary leading-relaxed text-body-color dark:text-cyan-200">
+                {currency}: {String(expense)}
+            </p>
+        </div>
+    )) : null;
+
     // TODO: Populate with all expenses + total of each currency spent in each trip
     // TODO: Add delete button for each expense (similar to deleteTrip component)
     return (
-        <div className='container mx-auto max-w-3xl p-8 grow'>
-            <h3>
-                <a className="mb-4 block text-xl font-semibold text-primary hover:text-primary dark:text-cyan-200 sm:text-[22px] md:text-xl lg:text-[22px] xl:text-xl 2xl:text-[22px]">
-                    {tripName}
-                </a>
-            </h3>
-            {/* Total Users Expense */}
-    
-            {/* Users Expense Paid */}
+        <>
+            <div className="mb-10 overflow-hidden rounded-lg bg-dark shadow-1 duration-300 hover:shadow-3 dark:bg-dark-2 dark:shadow-card dark:hover:shadow-3">
+                <div className="p-8 text-center mt-2 sm:p-9 md:p-7 xl:p-9">
+                    <h3>
+                        <a className="mb-4 block text-xl font-semibold text-primary hover:text-primary dark:text-cyan-200 sm:text-[22px] md:text-xl lg:text-[22px] xl:text-xl 2xl:text-[22px]">
+                            {tripName}
+                        </a>
+                    </h3>
+                    {/* Change this to grid view maybe */}
+                    <p className="mb-4 block text-primary hover:text-primary dark:text-cyan-200 sm:text-[22px] md:text-xl lg:text-[22px] xl:text-xl 2xl:text-[22px]">
+                        Total Spent this trip: {totalExpenseElements}
+                    </p>
+                    {/* Total Users Expense */}
 
-            {/* All Expenses */}
-        </div>
+                    {/* Users Expense Paid */}
+
+                    {/* All Expenses */}
+                </div>
+            </div>
+        </>
     )
 }
 
