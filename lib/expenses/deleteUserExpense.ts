1bbe936410dd339ac278adb8f7c078e27b7d907c
bryanleezh <leebryan307@gmail.com>
2024-04-19 20:19:19 +0200

    Feat: Add delete expense function to user collection

--- lib/expenses/deleteUserExpense.ts
+++ lib/expenses/deleteUserExpense.ts
@@ -1,10 +1,35 @@
 // * Function that deletes expense on user document
 
-import { doc, getFirestore } from "firebase/firestore";
+import { DocumentData, DocumentReference, doc, getDoc, getFirestore, updateDoc } from "firebase/firestore";
 import firebase_app from "../firebase/config";
 
-export default async function deleteUserExpense(uuid: string, currency: string, amount: number, user: string) {
+export default async function deleteUserExpense(user: string, currency: string, amount: number) {
     const db = getFirestore(firebase_app);
-    const userDocRef = doc(db, 'users', uuid);
-    
+    const userDocRef: DocumentReference<DocumentData, DocumentData> = doc(db, 'users', user);
+
+    try {
+        let userDocSnap = await getDoc(userDocRef);
+
+        if (userDocSnap.exists()) {
+            const userData = userDocSnap.data();
+            if (userData && userData.totalExpense) {
+                const totalExpense = userData.totalExpense;
+
+                if (totalExpense[currency]) {
+                    totalExpense[currency] -= amount;
+
+                    await updateDoc(userDocRef, { totalExpense });
+                    console.log(`Successfully decremented ${amount} from ${currency} in totalExpense for user ${user}!`);
+                } else {
+                    console.error(`Currency ${currency} not found in totalExpense!`);
+                }
+            } else {
+                console.error(`totalExpense not found in user document!`);
+            }
+        } else {
+            console.error(`User document for ${user} does not exist!`);
+        }
+    } catch (err) {
+        console.error("Error fetching user document: ", err);
+    }
 }
