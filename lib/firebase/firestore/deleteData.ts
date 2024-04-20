545053575b3b033aee0521e7c86831e4e63d07dd
bryanleezh <leebryan307@gmail.com>
2024-04-20 02:09:48 +0200

    Chore: Remove unneceessary code

--- lib/firebase/firestore/deleteData.ts
+++ lib/firebase/firestore/deleteData.ts
@@ -1,6 +1,6 @@
 import { DeleteResult } from "@/lib/types";
 import firebase_app from "../config";
-import { getFirestore, doc, deleteDoc, DocumentData } from "firebase/firestore";
+import { getFirestore, doc, deleteDoc } from "firebase/firestore";
 
 const db = getFirestore(firebase_app);
 
