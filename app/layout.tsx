8b7aac8c17d0389584da17c1b85b2834ffdac639
bryanleezh <leebryan307@gmail.com>
2024-04-21 23:28:53 +0200

    Change name of app to Money Pig

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,11 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import { AuthContextProvider } from '@/app/context/AuthContext';
-import { Nav } from '@/components';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Expenses',
+  title: 'Money Pig',
   description: 'Keep track of all expenses during trips/holidays',
 }
 
